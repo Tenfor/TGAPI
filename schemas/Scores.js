f790ab9b4c479d7b2454cba3ea842e4af4c3ec7f
Tenfor <f.zsolt95@gmail.com>
2025-06-29 13:43:03 +0200

    unique scores fix 2

--- schemas/Scores.js
+++ schemas/Scores.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const ScoreSchema = mongoose.Schema({
     playerName: {
         type: String,
-        required:true
+        required:true,
+        unique: true
     },
     scores: {
         type: Number,
